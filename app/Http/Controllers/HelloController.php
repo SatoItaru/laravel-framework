708d3c8905008416bbf76720190e95925e3631a8
SatoItaru <itaru1822@gmail.com>
2022-03-11 11:15:15 +0900

    template directive

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,27 +7,10 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response){
-        return <<<EOF
-        <html>
-        <head>
-        <title>Hello / Index</title>
-        <style>
-        body {font-size:16pt; color:#999;}
-        h1 {font-size:120pt; text-align:center; color:#fafafa; margin:-50px 0px 120px 0px;}
-        </style>
-        </head>
-        <body>
-        <h1>Hello</h1>
-        <h3>Request</h3>
-        <pre>{$request}</pre>
-        <h3>Response</h3>
-        <pre>{$response}</pre>
-        </body>
-        </html>
-        EOF;
+    public function index(Request $request){
 
-        $response->setContent($html);
-        return $response;
+        $data = ['one','two','three','four','five'];
+
+        return view('hello.index',['data'=>$data]);
     }
 }
