91bd6eda857f3ba530c223c2b7c3435134f4072a
SatoItaru <itaru1822@gmail.com>
2022-03-13 18:17:32 +0900

    validate basic processing

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
+        return view('hello.index', ['msg'=>'フォームを入力：']);
+    }
 
-        return view('hello.index');
+    public function post(Request $request)
+    {
+        $validate_rule = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric | between:0,150',
+        ];
+        $this->validate($request, $validate_rule);
+        return view('hello.index', ['msg'=>'正しく入力されました!']);
     }
 }
